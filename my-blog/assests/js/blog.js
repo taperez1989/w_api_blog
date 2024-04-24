459d9970b6481b58831812928ab1eea5609bea37
Ted Perez <taperez1989@yahoo.com>
2024-04-23 17:05:56 -0700

    light/dark mode code

--- my-blog/assests/js/blog.js
+++ my-blog/assests/js/blog.js
@@ -1,5 +1,20 @@
 // JSON.parse(localStorage.getItem("blogs"))
 
+const ldMode = document.querySelector('#ld-mode');
+const container = document.querySelector('.container');
+
+let mode = 'dark';
+
+ldMode.addEventListener('click', function () {
+    if (mode === 'dark') {
+        mode = 'light';
+        container.setAttribute('class', 'light');
+    }
+    else {
+        mode = 'dark';
+        container.setAttribute('class', 'dark');
+    }
+});
 
 const backButton = document.getElementById("#back-button");
 backButton = addEventListener('click', function () {
