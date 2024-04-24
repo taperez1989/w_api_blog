78aa6a437a12d56b42ffc627f7911fa7b8cc0ff1
Ted Perez <taperez1989@yahoo.com>
2024-04-23 22:51:15 -0700

    css work

--- my-blog/assests/js/blog.js
+++ my-blog/assests/js/blog.js
@@ -1,23 +1,8 @@
-// JSON.parse(localStorage.getItem("blogs"))
+JSON.parse(localStorage.getItem("blogs"))
 
-const ldMode = document.querySelector('#ld-mode');
-const container = document.querySelector('.container');
 
-let mode = 'dark';
-
-ldMode.addEventListener('click', function () {
-    if (mode === 'dark') {
-        mode = 'light';
-        container.setAttribute('class', 'light');
-    }
-    else {
-        mode = 'dark';
-        container.setAttribute('class', 'dark');
-    }
-});
-
-const backButton = document.getElementById("#back-button");
-backButton = addEventListener('click', function () {
+let backButton = document.getElementById("#back-button");
+backButton=addEventListener('click', function () {
     location.href = "index.html"
 });
 
